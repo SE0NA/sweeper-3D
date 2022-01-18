0cc0ffed0f0bc2e702bb74cc854cc52e2003599a
SE0NA <seona.git@gmail.com>
2022-01-18 21:37:34 +0900

    Decorates: Room 0~4
    Room 0~4 방 꾸밈

--- minsweeper/Assets/Scripts/Teleport.cs
+++ minsweeper/Assets/Scripts/Teleport.cs
@@ -45,5 +45,6 @@ public class Teleport : MonoBehaviour
                 transform.GetChild(roomNum).GetComponent<Image>().color = Color.cyan;
                 break;
         }
+        Debug.Log("»ö ¹Ù²ñ");
     }
 }
