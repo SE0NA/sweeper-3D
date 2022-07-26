b0cf4d361732df04bd0efd76cf6df53c4489b71a
SE0NA <85846475+SE0NA@users.noreply.github.com>
2022-07-26 22:02:39 +0900

    MainScene - 페이드 애니메이션
    게임 실행, 게임 시작 및 종료에 대한 페이드 인, 페이드 아웃 애니메이션 적용

--- minsweeper/Assets/Scripts/MainManager.cs
+++ minsweeper/Assets/Scripts/MainManager.cs
@@ -5,14 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MainManager : MonoBehaviour
 {
+    [SerializeField] GameObject uiCanvas;
+    Animator uiAnimator;
+
+    private void Start()
+    {
+        uiAnimator = uiCanvas.GetComponent<Animator>();
+    }
+
     public void Btn_MultiGame()
     {
-        SceneManager.LoadScene("JoinLobby");
+        uiAnimator.Play("Main_Fade Out");
+        Invoke("Game_Invoke", 1f);
     }
+    void Game_Invoke() => SceneManager.LoadScene("JoinLobby");
 
     public void Extra_join()
     {
         SceneManager.LoadScene("Lobby_extra");
     }
 
+    public void Btn_Quit()
+    {
+        uiAnimator.Play("Main_Fade Out");
+        Invoke("Quit_Invoke", 1f);
+    }
+    void Quit_Invoke() => Application.Quit();
 }
